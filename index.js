e0a238a97abf5825a15103d0d2953655d0b05f8d
Quanluis <quanluis@protonmail.com>
2019-03-11 17:48:36 -0400

    First commit, created basic html skeleton and targeted the dom buttons.

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log('Is this working?')
+
+let x = document.getElementById('add').onclick = function() {
+    console.log('you add me')
+}
+
+let y = document.getElementById('sub').onclick = function() {
+    console.log('You sub me')
+}
+
+
+
+
